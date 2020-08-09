35e3b4b3e1b04a3ccea26de2b118d20def6ebe3c
Malgorzata Berdys <berdysm@gmail.com>
2020-08-09 12:28:52 +0200

    fINISH TASK 12.5

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,17 +1,23 @@
 // selectors
 export const getSearchString = ({searchString}) => searchString;
-export const countAllCards = ({cards}) => cards.lenght;
+export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const SEARCH_CARD = createActionName('SEARCH_CARD');
 
 // action creators
+export const createAction_changeSearchString = (payload) => ({ payload, type: SEARCH_CARD });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case SEARCH_CARD:
+      return action.payload;
     default:
       return statePart;
   }
